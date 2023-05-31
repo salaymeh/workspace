5906cff51e4a44b1bbdca665569ad12b3c525da9
Ahmad Salaymeh <ahmadsalaymeh@Ahmads-Mac-mini.local>
2023-05-31 19:58:50 -0400

    pre-commit

---
+++ learn_django/django_pt1/chapter_7/blog/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.views.generic import DeleteView
+from django.views.generic import DetailView
+from django.views.generic import ListView
+from django.views.generic import UpdateView
+
+from .models import Post
+
+# Create your views here.
+
+
+class BlogListView(ListView):
+    model = Post
+    template_name = "home.html"
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title", "author", "body"]
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["title", "body"]
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
